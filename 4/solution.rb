e3a098eb516aa2d951ffefb52298208cb2f6c181
Tyumici <148009727+tyumici@users.noreply.github.com>
2024-12-04 13:28:23 -0500

    day 4 part 1

--- 4/solution.rb
+++ 4/solution.rb
@@ -1,1 +1,44 @@
-input = IO.read('/home/human/Documents/GIT/aoc_2024/4/input.txt').lines.map(&:chomp)
+input = IO.read('/home/human/Documents/GIT/aoc_2024/4/input.txt')
+$input_clean = File.readlines('/home/human/Documents/GIT/aoc_2024/4/input.txt').map(&:chomp)
+$word = "XMAS"
+$mass_x = "MAS"
+$directions = [
+  [0, -1],  # left
+  [0, 1],   # right
+  [1, 0],   # down
+  [1, -1],   # up-right
+  [1, 1],   # down-right
+  [-1, 1],  # down-left
+  [-1, -1], # up-left
+  [-1, 0],  # up
+]
+
+$part_one = 0
+$part_two = 0
+
+def find_word(grid, start_row, start_col, direction)
+
+  (0...$word.length).each do |i|
+    new_row = start_row + i * direction[0]
+    new_col = start_col + i * direction[1]
+    return false if new_row < 0 || new_row >= grid.length || new_col < 0 || new_col >= grid[0].length
+    return false if grid[new_row][new_col] != $word[i]
+  end
+
+  true
+end
+
+def part1(input)
+  (0...input.length).each do |row|
+    (0...input[0].length).each do |col|
+      $directions.each do |dir|
+        if find_word(input, row, col, dir)
+          $part_one += 1
+        end
+      end
+    end
+  end
+  puts $part_one
+end
+
+part1($input_clean) # 2401
